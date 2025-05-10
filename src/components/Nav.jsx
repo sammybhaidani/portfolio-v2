ba4fb1aa18abcf3c37bf4243314255cb3608be80
sammy <sammybhaidani@gmail.com>
2025-05-11 00:30:13 +0100

    Add nav links for different sections

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,6 +5,9 @@ export default function Nav() {
         <nav>
             <ul>
                 <li><NavItem text={'Home'} address={'/'}/></li>
+                <li><NavItem text={'About'} address={'/portfolio#about'}/></li>
+                <li><NavItem text={'Projects'} address={'/portfolio#projects'}/></li>
+                <li><NavItem text={'Contact'} address={'/portfolio#contact'}/></li>
             </ul>
         </nav>
     )
