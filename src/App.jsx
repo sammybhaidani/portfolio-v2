519a28f6a2515d733c1cbb2529b712e4490eabd8
sammy <sammybhaidani@gmail.com>
2025-05-10 23:04:44 +0100

    Create route for home page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Nav from "./components/Nav";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Nav/>
+            <Routes>
+                <Route path='/' element={<HomePage/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
