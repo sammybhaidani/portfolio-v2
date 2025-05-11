b2c059dba0a073842ef729092343ad5bbd3e865d
sammy <sammybhaidani@gmail.com>
2025-05-11 06:28:17 +0100

    Add styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import NavItem from "./atoms/NavItem";
 export default function Nav() {
     return (
         <nav>
-            <ul>
+            <ul className="flex gap-3">
                 <li><NavItem text={'Home'} address={'/'}/></li>
                 <li><NavItem text={'About'} address={'/portfolio#about'}/></li>
             </ul>
