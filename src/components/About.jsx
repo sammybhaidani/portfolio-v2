6eb4eefda43d38ff4c3916e34fe8496aefe4d96c
sammy <sammybhaidani@gmail.com>
2025-05-11 06:58:44 +0100

    Implement section heading component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,9 @@
+import SectionHeading from "./atoms/SectionHeading";
+
 export default function About() {
     return (
         <div>
-            <h2>About</h2>
+            <SectionHeading text={'About'}/>
             <p>
                 I'm a developer, passionate about building engaging experiences for the web.
                 Crafting a seamless user experience that involves great design and is well optimised 
