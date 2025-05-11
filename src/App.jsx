d562934cca9d57ba820eebe7d4770b824b689ea7
sammy <sammybhaidani@gmail.com>
2025-05-11 06:58:01 +0100

    Remove react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Nav from "./components/Nav";
-import PortfolioPage from "./pages/PortfolioPage";
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Nav/>
-            <Routes>
-                <Route path='/' element={<HomePage/>}/>
-                <Route path='/portfolio' element={<PortfolioPage/>}/>
-            </Routes>
-        </BrowserRouter>
+        <>
+        <Nav/>
+        <HomePage/>
+        </>         
     )
 }
