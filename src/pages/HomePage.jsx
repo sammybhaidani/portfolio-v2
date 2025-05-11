7610855ef51596216301faaad7bce3b72f2ccef4
sammy <sammybhaidani@gmail.com>
2025-05-12 00:16:09 +0100

    Add components to home page and some styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,20 @@
+import About from "../components/About";
+import MainTitle from "../components/atoms/MainTitle";
+import Contact from "../components/Contact";
+import Projects from "../components/Projects";
+
 export default function HomePage() {
     return (
         <>
-        
+        <div className="flex flex-col gap-3">
+        <MainTitle text={'Sammy Bhaidani'}/>
+        <p className="font-bold">Software Engineer</p>
+        <p>I like to create engaging experiences on the web.</p>
+        </div>
+
+        <About/>
+        <Projects/>
+        <Contact/>
         </>
     )
 }
