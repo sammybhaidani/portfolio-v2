164925e9ff9da362e550897752a6bb2c739556e4
sammy <sammybhaidani@gmail.com>
2025-05-11 06:32:23 +0100

    Add initial structure for portfolio sections

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+export default function About() {
+    return (
+        <div>
+            <h2>About</h2>
+            <p>
+                I'm a developer, passionate about building engaging experiences for the web.
+                Crafting a seamless user experience that involves great design and is well optimised 
+                for performance is one of my favourite parts of development.
+            </p>
+        </div>
+    )
+}
