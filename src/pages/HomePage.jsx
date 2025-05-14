867ac02844599a5849d490adb049327fbb741eea
sammy <sammybhaidani@gmail.com>
2025-05-14 04:40:05 +0100

    Add Hero component and move hero section content into it

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,12 @@
 import About from "../components/About";
-import MainTitle from "../components/atoms/MainTitle";
 import Contact from "../components/Contact";
 import Projects from "../components/Projects";
+import Hero from "../components/Hero";
 
 export default function HomePage() {
     return (
         <>
-        <div className="flex flex-col gap-3">
-        <MainTitle text={'Sammy Bhaidani'}/>
-        <p className="font-bold">Software Engineer</p>
-        <p>I like to create engaging experiences on the web.</p>
-        </div>
-
+        <Hero/>
         <About/>
         <Projects/>
         <Contact/>
