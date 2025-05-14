d39957482a6199a5f7e91c10f8d94cb8d2433ef6
sammy <sammybhaidani@gmail.com>
2025-05-14 04:36:10 +0100

    Add Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import HomePage from "./pages/HomePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFoundPage from "./pages/NotFoundPage";
+import Nav from "./components/Nav";
 
 export default function App() {
     return (
         <>
         <BrowserRouter>
+        <Nav/>
             <Routes>
                 <Route index element={<HomePage/>}/>
                 <Route path="*" element={<NotFoundPage/>}/>
