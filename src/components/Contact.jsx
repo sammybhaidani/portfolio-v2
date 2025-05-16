43dd46e6c016cb3d97a75e0ec298d88be1dfbf5f
sammy <sammybhaidani@gmail.com>
2025-05-16 02:22:34 +0100

    Add semantic html and styling for contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,8 @@ import SectionHeading from "./atoms/SectionHeading";
 
 export default function Contact() {
     return (
-        <>
+        <section className="min-h-screen py-10 mx-auto max-w-6xl">
         <SectionHeading text={'Contact'}/>
-        </>
+        </section>
     )
 }
